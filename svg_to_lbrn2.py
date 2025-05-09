7a254db838ad3cf1e719884b27b60e060482b1e5
Thomas Langewouters <thomas.langewouters@thouters.be>
2025-05-09 20:59:47 +0200

    update

--- svg_to_lbrn2.py
+++ svg_to_lbrn2.py
@@ -1,8 +1,19 @@
 import xml.etree.ElementTree as ET
 from xml.dom import minidom
+import io
 import os
+from PIL import Image
+import base64
+from typing import List, Tuple  
 
-def parse_svg(svg_file):
+origin_x = 0
+origin_y = 0
+board_width_mm = 51
+board_height_mm = 71
+
+
+img_dpi = 300
+def parse_svg(svg_file) -> Tuple[dict,List[dict]]:
     """Parse the SVG file and extract shapes."""
     tree = ET.parse(svg_file)
     root = tree.getroot()
@@ -21,7 +32,16 @@ def parse_svg(svg_file):
         r = float(circle.attrib.get('r', '0'))
         shapes.append({'type': 'Ellipse', 'cx': cx, 'cy': cy, 'rx': r, 'ry': r})
 
-    return shapes
+    # width="29.1846mm" height="23.1902mm"
+    # Extract width and height from the SVG root element
+    width = float(root.attrib.get('width', '0').replace('mm', ''))
+    height = float(root.attrib.get('height', '0').replace('mm', ''))
+    info = {
+        'Width': width,
+        'Height': height,
+    }
+
+    return (info,shapes)
 
 def format_vertlist(d):
     """Format the VertList to include prefixes and control points."""
@@ -48,32 +68,91 @@ def create_lbrn2(filenames, output_file):
 
     # Add CutSettings for each layer
     for index, filename in enumerate(filenames):
-        cut_setting = ET.SubElement(project, 'CutSetting', {'type': 'Cut'})
-        ET.SubElement(cut_setting, 'index', {'Value': str(index)})
-        ET.SubElement(cut_setting, 'name', {'Value': os.path.basename(filename)})
-        ET.SubElement(cut_setting, 'maxPower', {'Value': '20'})
-        ET.SubElement(cut_setting, 'maxPower2', {'Value': '20'})
-        ET.SubElement(cut_setting, 'speed', {'Value': '100'})
-        ET.SubElement(cut_setting, 'priority', {'Value': '0'})
+        if filename.endswith('.svg'):
+            cut_setting = ET.SubElement(project, 'CutSetting', {'type': 'Cut'})
+            ET.SubElement(cut_setting, 'index', {'Value': str(index)})
+            ET.SubElement(cut_setting, 'name', {'Value': os.path.basename(filename)})
+            ET.SubElement(cut_setting, 'maxPower', {'Value': '20'})
+            ET.SubElement(cut_setting, 'maxPower2', {'Value': '20'})
+            ET.SubElement(cut_setting, 'speed', {'Value': '100'})
+            ET.SubElement(cut_setting, 'priority', {'Value': '0'})
+        elif filename.endswith('.png'):
+            cut_setting_img = ET.SubElement(project, 'CutSetting_Img', {'type': 'Image'})
+            ET.SubElement(cut_setting_img, 'index', {'Value': str(index)})
+            ET.SubElement(cut_setting_img, 'name', {'Value': os.path.basename(filename)})
+            ET.SubElement(cut_setting_img, 'maxPower', {'Value': '33'})
+            ET.SubElement(cut_setting_img, 'maxPower2', {'Value': '20'})
+            # convert speed from mm/sec to mm/min:
+            speed_mm_sec = 5
+            speed = speed_mm_sec / 60
+            ET.SubElement(cut_setting_img, 'speed', {'Value': str(speed)})
+            ET.SubElement(cut_setting_img, 'bidir', {'Value': '0'})
+            line_interval_mm = "0.0847"
+            ET.SubElement(cut_setting_img, 'interval', {'Value': line_interval_mm})
+            ET.SubElement(cut_setting_img, 'priority', {'Value': '0'})
+            ET.SubElement(cut_setting_img, 'tabCount', {'Value': '1'})
+            ET.SubElement(cut_setting_img, 'tabCountMax', {'Value': '1'})
+            ET.SubElement(cut_setting_img, 'ditherMode', {'Value': 'threshold'})
+            ET.SubElement(cut_setting_img, 'dpi', {'Value': str(img_dpi)})
 
     for index, filename in enumerate(filenames):
-        group = ET.SubElement(project, 'Shape', {'Type': 'Group'})
-        ET.SubElement(group, 'XForm').text = '1 0 0 1 0 0'
-        children = ET.SubElement(group, 'Children')
-
         #python match statement to execute based on file extension (.svg, .png):
         if filename.endswith('.svg'):
-            shapes = parse_svg(filename)
+            info, shapes = parse_svg(filename)
+            width_mm = info['Width']
+            height_mm = info['Height']
+            x = origin_x + board_width_mm/2 - width_mm/2
+            y = origin_y + height_mm/2 + board_height_mm/2
+            group = ET.SubElement(project, 'Shape', {'Type': 'Group'})
+            ET.SubElement(group, 'XForm').text = f'1 0 0 1 {x} {y}'
+            children = ET.SubElement(group, 'Children')
+
+
             for shape in shapes:
                 if shape['type'] == 'Path':
                     shape_elem = ET.SubElement(children, 'Shape', {'Type': 'Path', 'CutIndex': str(index)})
-                    ET.SubElement(shape_elem, 'XForm').text = '1 0 0 1 0 0'
+                    ET.SubElement(shape_elem, 'XForm').text = f'1 0 0 1 0 0'
                     ET.SubElement(shape_elem, 'VertList').text = format_vertlist(shape['d'])
                     ET.SubElement(shape_elem, 'PrimList').text = 'LineClosed'
                 elif shape['type'] == 'Ellipse':
                     shape_elem = ET.SubElement(children, 'Shape', {'Type': 'Ellipse', 'CutIndex': str(index), 'Rx': str(shape['rx']), 'Ry': str(shape['ry']) })
                     ET.SubElement(shape_elem, 'XForm').text = f"1 0 0 -1 {shape['cx']} -{shape['cy']}"
         elif filename.endswith('.png'):
+            x = origin_x
+            y = origin_y
+            group = ET.SubElement(project, 'Shape', {'Type': 'Group'})
+            ET.SubElement(group, 'XForm').text = f'1 0 0 1 {x} {y}'
+            children = ET.SubElement(group, 'Children')
+
+
+            # Open the image file
+            with Image.open(filename) as img:
+                width, height = img.size
+                # Get the DPI (dots per inch) of the image
+                img_dpi_read = img.info.get('dpi', (img_dpi, img_dpi))[0]
+                mirrored_img = img.transpose(Image.FLIP_TOP_BOTTOM)
+                
+                # Save the mirrored image to a BytesIO buffer
+                buffer = io.BytesIO()
+                mirrored_img.save(buffer, format="PNG")
+                
+                # Encode the buffer's content to a base64 string
+                base64_string = base64.b64encode(buffer.getvalue()).decode('utf-8')
+
+
+            data = {'Type': 'Bitmap', 'CutIndex': str(index)}
+            #add the width of the image to data['W']:
+            def to_mm(value, dpi):
+                return value * 25.4 / dpi
+            height_mm = to_mm(height, img_dpi_read)
+            width_mm = to_mm(width, img_dpi_read)
+            data['W'] = str(width_mm)
+            data['H'] = str(height_mm)
+            x = origin_x + width_mm/2
+            y = origin_y + height_mm/2
+            data['Data'] = base64_string 
+            shape_elem = ET.SubElement(children, 'Shape', data)
+            ET.SubElement(shape_elem, 'XForm').text = f'1 0 0 1 {str(x)} {str(y)}'
 
     # Write to file
     xml_str = minidom.parseString(ET.tostring(project)).toprettyxml(indent="    ")
@@ -85,6 +164,7 @@ if __name__ == '__main__':
         '/home/thomas/pets/sculpfun/svg2lbrn/pir-24v-F_Mask.svg',
 #        '/home/thomas/pets/sculpfun/svg2lbrn/pir-24v-F_Silkscreen.svg',
 #        '/home/thomas/pets/sculpfun/svg2lbrn/pir-24v-B_Mask.svg',
+        '/home/thomas/pets/sculpfun/svg2lbrn/pir-24v-F_Cu.png',
         # Add more SVG file paths here if needed
     ]
     output_file = '/home/thomas/pets/sculpfun/svg2lbrn/out.lbrn2'
